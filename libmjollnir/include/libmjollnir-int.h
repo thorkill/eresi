22601c312a8719122c3724bd55f49d99705eae9b
thor <gh-eighae6A@eresi-project.org>
2006-02-23 22:42:25 +0000

    Import of elfsh-0.7a6p3rc2-20060223

--- libmjollnir/include/libmjollnir-int.h
+++ libmjollnir/include/libmjollnir-int.h
@@ -2,7 +2,7 @@
 /*
  * (C) 2006 Asgard Labs, thorolf a grid.einherjar.de
  * BSD License
- * $Id: libmjollnir-int.h,v 1.1.1.1 2006-02-20 23:25:27 thor Exp $
+ * $Id: libmjollnir-int.h,v 1.1.1.2 2006-02-23 22:42:25 thor Exp $
  */
 
 #define __DEBUG__ 0
@@ -10,8 +10,8 @@
 void * mjr_fingerprint_function(unsigned char *buff,int type, int mlen);
 
 /* this is very beta early version - so don't panic  WIP*/
-int mjr_find_calls_raw(elfshobj_t *,hash_t *,char *,char *,int *);
-int mjr_find_calls_symtab(elfshobj_t *,hash_t *,int *);
+int mjr_find_calls_raw(Mjr_CTX *,hash_t *,char *,char *);
+int mjr_find_calls_symtab(elfshobj_t *,hash_t *);
 
 /* libmjollnir.c */
 int mjr_i386_cut_function(unsigned char *src,unsigned char *dst, int mlen);
