2e44c9ee4177cb4c638dfdc113ea37fa62e585c1
may <gh-eiC3ahsh@eresi-project.org>
2006-07-07 14:57:21 +0000

    Added e2dbg/threads.c

---
+++ e2dbg/threads.c
@@ -0,0 +1,136 @@
+/*
+** threads.c for e2dbg
+**
+** Hook the threads API for keeping thread debugger information accurate
+**
+** Last Update Thu July 06 19:37:26 2006 mm
+*/
+#include "elfsh.h"
+#include <pthread.h>
+
+
+/* Hooked start routine for all threads */
+static void*		e2dbg_thread_start(void *param)
+{
+  e2dbgthread_t	*cur;
+  void*		(*start)(void *param);
+  char		key[15];
+  
+  snprintf(key, sizeof(key), "%u", (unsigned int) pthread_self());
+  cur = hash_get(&e2dbgworld.threads, key);
+
+#if __DEBUG_THREADS__
+  printf(" [*] Starting thread ID %u \n", (unsigned int) pthread_self());
+#endif
+
+  cur->state = E2DBG_THREAD_STARTED;
+
+  start = (void *) cur->entry;
+  return ((*start)(param));
+}
+
+
+/* Stop all threads when a breakpoint happens */
+void		e2dbg_thread_stopall()
+{
+  hashent_t     *actual;
+  e2dbgthread_t	*cur;
+  u_int         index;
+
+#if __DEBUG_THREADS__
+  printf(" [*] Stopping all threads \n");
+#endif
+
+  for (index = 0; index < e2dbgworld.threads.size; index++)
+    for (actual = &e2dbgworld.threads.ent[index]; 
+	 actual != NULL && actual->key != NULL; actual = actual->next)
+      {
+	cur = actual->data;
+	if (cur->tid != vm_dbgid_get() && cur->tid != pthread_self())
+	  {
+	    pthread_kill(cur->tid, SIGSTOP);
+#if __DEBUG_THREADS__
+	    printf(" [*] Stopping thread ID %u \n", (unsigned int) cur->tid);
+#endif
+	  }
+      }
+}
+
+
+/* Continue all threads after a breakpoint */
+void		e2dbg_thread_contall()
+{
+  hashent_t     *actual;
+  e2dbgthread_t	*cur;
+  u_int         index;
+
+#if __DEBUG_THREADS__
+  printf(" [*] Continuing all threads \n");
+#endif
+
+  for (index = 0; index < e2dbgworld.threads.size; index++)
+    for (actual = &e2dbgworld.threads.ent[index]; 
+	 actual != NULL && actual->key != NULL; actual = actual->next)
+      {
+	cur = actual->data;
+	if (cur->tid != vm_dbgid_get())
+	  {
+#if __DEBUG_THREADS__
+	    printf(" [*] Continuing thread ID %u \n", (unsigned int) cur->tid);
+#endif
+	    pthread_kill(cur->tid, SIGCONT);
+	  }
+      }
+}
+
+
+
+/* Hook for pthread_create */
+int		pthread_create (pthread_t *__restrict __threadp,
+				__const pthread_attr_t *__restrict __attr,
+				void *(*__start_routine) (void *),
+				void *__restrict __arg)
+{
+  e2dbgthread_t	*new;
+  char		*key;
+  int		ret;
+  int		(*fct)(pthread_t *__restrict __threadp, 
+		       __const pthread_attr_t *__restrict __attr,
+		       void *__start_routine,
+		       void *__restrict __arg);
+
+  ELFSH_NOPROFILE_IN();
+
+  fct = (void *) e2dbgworld.pthreadcreate;
+  ret = (*fct)(__threadp, __attr, e2dbg_thread_start, __arg);
+  XALLOC(new, sizeof(e2dbgthread_t), -1);
+  XALLOC(key, 15, -1);
+  snprintf(key, 15, "%u", *(unsigned int *) __threadp);
+  new->tid   = *(unsigned long *) __threadp;
+  new->entry = __start_routine;
+  time(&new->stime);
+  hash_add(&e2dbgworld.threads, key, new);
+  ELFSH_NOPROFILE_ROUT(ret);
+}
+
+
+/* Hook for pthread_exit */
+void	pthread_exit(void *retval)
+{
+  void	(*fct)(void *retval);
+
+  fct = (void *) e2dbgworld.pthreadexit;
+
+#if __DEBUG_THREADS__
+  printf("PTHREAD_EXIT ! Calling back original at %08X \n", (elfsh_Addr) fct);
+#endif
+
+  (*fct)(retval);
+  while (1)
+    sleep(1);
+}
+
+
+
+
+
