bb5859d72bc78a96efa29df6ac9f269ce49bd0c9
heroine <gh-ohZ7phee@eresi-project.org>
2007-05-09 16:26:23 +0000

    Fixed a bug in operand interpretation. Type fetched was GENERAL instead of ENCODED.

--- libasm/src/arch/ia32/handlers/op_lea_rv_m.c
+++ libasm/src/arch/ia32/handlers/op_lea_rv_m.c
@@ -1,6 +1,6 @@
 /**
  * @file op_lea_rv_m
- * $Id: op_lea_rv_m.c,v 1.2 2007-04-13 06:56:34 heroine Exp $
+ * $Id: op_lea_rv_m.c,v 1.3 2007-05-09 16:26:23 heroine Exp $
  *
  */
 #include <libasm.h>
@@ -13,14 +13,13 @@
 */
 
 int op_lea_rv_m(asm_instr *new, u_char *opcode, u_int len, asm_processor *proc) {
-  
   new->len += 1;
   new->ptr_instr = opcode;
   new->instr = ASM_LEA;
 
 #if LIBASM_USE_OPERAND_VECTOR
-  new->len += asm_operand_fetch(&new->op1, opcode + 1, ASM_OTYPE_GENERAL, proc);
-  new->len += asm_operand_fetch(&new->op2, opcode + 1, ASM_OTYPE_MEMORY, proc);
+  new->len += asm_operand_fetch(&new->op1, opcode + 1, ASM_OTYPE_ENCODED, proc);
+  new->len += asm_operand_fetch(&new->op2, opcode + 1 + new->op1.len, ASM_OTYPE_MEMORY, proc);
 #else
   new->op1.type = ASM_OTYPE_GENERAL;
   new->op1.size = ASM_OSIZE_VECTOR;
