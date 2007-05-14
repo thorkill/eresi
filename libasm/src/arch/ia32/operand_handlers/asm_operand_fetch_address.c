b7847c9e06cd48031f803831d579c22a1bf936e6
heroine <gh-ohZ7phee@eresi-project.org>
2007-05-14 12:03:41 +0000

    asm_operand_fetch.c

--- libasm/src/arch/ia32/operand_handlers/asm_operand_fetch_address.c
+++ libasm/src/arch/ia32/operand_handlers/asm_operand_fetch_address.c
@@ -1,12 +1,18 @@
 /**
  * @file asm_operand_fetch.c
- * $Id: asm_operand_fetch_address.c,v 1.1 2007-05-09 17:39:35 heroine Exp $
+ * $Id: asm_operand_fetch_address.c,v 1.2 2007-05-14 12:03:41 heroine Exp $
  */
 
 #include <libasm.h>
 #include <libasm-int.h>
 
-int     asm_operand_fetch_address(asm_operand *operand, u_char *opcode, int otype, 
+/**
+ * Fetch operands of 
+ *
+ *
+ */
+int     asm_operand_fetch_address(asm_operand *operand, u_char *opcode, 
+				  int otype, 
                                   asm_processor *proc)
 {
   operand->type = ASM_OTYPE_ADDRESS;
