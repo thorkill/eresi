134bd631d8d919f2fdc21f2dd0ea2aeaad787291
heroine <gh-ohZ7phee@eresi-project.org>
2007-05-30 00:49:25 +0000

    Code cleanup. Minimal error handling, returns -1 on errors. todo: specific message handling by error.

--- libasm/src/arch/ia32/handlers/op_386sp.c
+++ libasm/src/arch/ia32/handlers/op_386sp.c
@@ -1,66 +1,47 @@
 /** 
  * @file op_386sp.c
- * $Id: op_386sp.c,v 1.5 2007-05-30 00:15:41 heroine Exp $
+ * $Id: op_386sp.c,v 1.6 2007-05-30 00:49:25 heroine Exp $
  * ChangeLog:
  * 2007-05-30	Fixed a bug in fetching. The vector used was the previously defined.
  *		strauss set up a new disasm vector and didn't know about it which 
  *		was called here.
- *
+ *		Filled instruction opcode pointer.
+ *		Removed the old unused handler.
+ *		Added minimal error management.
  */
 #include <libasm.h>
 #include <libasm-int.h>
 
-#if LIBASM_USE_VECTOR == 0
-
-/*
- * Opcode :             0x0f
- * Following byte must be fetched from i386 instruction table.
- */
-
-int op_386sp(asm_instr *new, u_char *opcode, u_int len, asm_processor *proc)
-{
-  asm_i386_processor    *i386p;
-  
-  /* this is a bridge to i386 instructions, pentium, ... instructions */
-  new->ptr_instr = opcode;
-  i386p = (asm_i386_processor *) proc;
-  if (i386p->internals->cisc_i386[*(opcode + 1)]) {
-    new->len += 1;
-    return (i386p->internals->cisc_i386[*(opcode + 1)](new, opcode + 1, 
-                                                      len - 1, proc));  
-  }
-  else
-    return (0);
-}
-
-#else
 
 /**
- * This is the handler which will find in the vector for extended instructions
- * which are encoded on 2 bytes.
- * The second byte
- * @param
+ * This is the handler for 2 bytes instruction, opcode 0x0f
+ * The second byte is used to fetch a new handler in the vector
+ * starting at offset 0x100.
+ * @param ins Pointer to instruction structure.
+ * @param buf Pointer to data to disassemble.
+ * @param len Length of dat to disassemble.
+ * @param proc Pointer to processor structure.
+ * @return Length of instruction or -1 error.
  */
 
 int     op_386sp(asm_instr *ins, u_char *buf, u_int len, asm_processor *proc)
 { 
   vector_t      *vec;
-  u_int         dim[1]; // dim[4];
+  u_int         dim[1];
   u_char        opcode;
   int           (*fetch)(asm_instr *, u_char *, u_int, asm_processor *);
   
-  vec = aspect_vector_get("disasm");
+  if (len < 2)
+    return (-1);
+  if (!(vec = aspect_vector_get("disasm")))
+    return (-1);
   opcode = *(buf + 1);
   dim[0] = opcode + 0x100;
-  /*
-  dim[0] = LIBASM_VECTOR_IA32;
-  dim[1] = opcode + 0x100;
-  dim[2] = 0;
-  dim[3] = 0;
-  */
-  fetch = aspect_vectors_select(vec, dim);
+  if (!(fetch = aspect_vectors_select(vec, dim)))
+    return (-1);
+  if (!ins->ptr_instr)
+    ins->ptr_instr = buf;
   ins->len += 1;
   return (fetch(ins, buf + 1, len - 1, proc));
 }
 
-#endif
