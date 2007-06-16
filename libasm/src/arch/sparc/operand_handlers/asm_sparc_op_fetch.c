1a4ef4c07b7944457d0e0a6d00e72c57953a6b48
strauss <gh-ahPei0Sh@eresi-project.org>
2007-06-16 20:30:22 +0000

    Missing the new files.

---
+++ libasm/src/arch/sparc/operand_handlers/asm_sparc_op_fetch.c
@@ -0,0 +1,34 @@
+#include <libasm.h>
+#include <libasm-int.h>
+
+/**
+ * Main function, dispatch processing to handler.
+ * @param op Pointer to operand to fill
+ * @param opcode Pointer to operand data.
+ * @param otype Type of operand to fetch : ASM_OTYPE_*
+ * @param proc Pointer to processor structure.
+ * @return Operand length or -1 on error (should currently never occur)
+ */
+
+int asm_sparc_op_fetch(asm_operand *op, u_char *opcode, int otype, 
+                 			  asm_instr *ins)
+{ 
+  vector_t      *vec;
+  u_int         dim[1];
+  int           to_ret;
+  int           (*fetch)(asm_operand *, u_char *, int, asm_instr *);
+  
+  vec = aspect_vector_get("operand-sparc");
+  dim[0] = otype;
+  
+  fetch = aspect_vectors_select(vec, dim);
+  to_ret = fetch(op, opcode, otype, ins);
+
+  if (to_ret == -1)
+  {
+    printf("%s:%i Unsupported operand type : %i\n", __FILE__, __LINE__, 
+    	     otype);
+  }
+
+  return (to_ret);
+}
---
+++ libasm/src/arch/sparc/operand_handlers/asm_sparc_op_fetch_register.c
@@ -0,0 +1,20 @@
+#include <libasm.h>
+#include <libasm-sparc.h>
+
+/**
+ * Decode data for operand type ASM_SP_OTYPE_REGISTER
+ * @param operand Pointer to operand structure to fill.
+ * @param opcode Pointer to opcode data.
+ * @param otype Operand type.
+ * @param ins Pointer to instruction structure.
+ *
+ * @return Operand length (currently unused in libasm-sparc)
+ */
+
+int asm_sparc_op_fetch_register(asm_operand *operand, u_char *opcode, 
+                       				   int otype, asm_instr *ins)
+{
+  operand->type = ASM_SP_OTYPE_REGISTER;
+
+  return (0);
+}
