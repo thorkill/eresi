92a632f8bf747a8cd8eae11830acc6230219c55a
pi3 <gh-Ooz8ooda@eresi-project.org>
2008-03-05 12:44:37 +0000

    Lost files...

---
+++ libasm/src/arch/mips/operand_handlers/asm_mips_operand_fetch.c
@@ -0,0 +1,29 @@
+/*
+ * - Adam 'pi3' Zabrocki
+ *
+ */
+
+#include <libasm.h>
+
+int asm_mips_operand_fetch(asm_operand *op, u_char *opcode, int otype,
+                                          asm_instr *ins)
+
+{ 
+  vector_t      *vec;
+  u_int         dim[1];
+  int           ret;
+  int           (*func_op)(asm_operand *op, u_char *opcode, int otype, asm_instr *ins);
+  
+  vec = aspect_vector_get(LIBASM_VECTOR_OPERAND_MIPS);
+  dim[0] = otype;
+  
+  func_op = aspect_vectors_select(vec, dim);
+  if ( (ret = func_op(op, opcode, otype, ins)) == -1) {
+    printf("%s:%i Unsupported operand content : %i\n", __FILE__, __LINE__, 
+    	     otype);
+  }
+
+//  op->name = asm_sparc_get_op_name(op);
+
+  return (ret);
+}
