8fc2b83d08f8cc44d2a62b97920a49cee04ee48e
enioh <gh-toec0eiL@eresi-project.org>
2008-05-02 01:09:15 +0000

    summary: Testsuite compilation fix.
    Some testuite directories were not being linked
    with etrace.
    
    Note about this:
    
    It sounds strange to me to need to link with
    libetrace just to use elfsh features like in
    ctors_hijack.
    
    If libelfsh is depending on libetrace, this should
    be corrected.
    
    The env.sh script is also adding to PATH
    the directories of elfsh, etrace and kernsh.

--- env.sh
+++ env.sh
@@ -18,5 +18,9 @@ export LD_LIBRARY_PATH=`pwd`/testsuite/e2dbg_multithreads/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/lib/debug/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/libkernsh/:$LD_LIBRARY_PATH
 
+export PATH=`pwd`/elfsh:$PATH
+export PATH=`pwd`/etrace:$PATH
+export PATH=`pwd`/e2dbg:$PATH
+export PATH=`pwd`/kernsh:$PATH
 
 echo Environment now ready for testing ERESI without install
