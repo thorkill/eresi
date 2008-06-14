e4631742a7f1dc0f9f070acaa2fc2c4f0f4fa9bb
enioh <gh-toec0eiL@eresi-project.org>
2008-06-14 15:43:40 +0000

    summary: Testsuite for libelfsh [include bug unit test]
    This testsuite simply load a binary and extract .text section.
    There is a bug on loading of object files when trying to load data
    located at virtual address 0x0.
    
    This testsuite include a check for that bug.

---
+++ testsuite/elf/extract_raw_data/load.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <libelfsh.h>
+
+int	test_binary(char *bin);
+
+/**
+ *
+ *
+ */
+int	main(int ac, char **av)
+{
+  int	idx;
+  
+  char *targets[] =
+    {
+      "load",
+      "load.o",
+      NULL
+    };
+  
+  if (ac > 1)
+    test_binary(av[1]);
+  else
+    {
+      for (idx = 0; targets[idx] != NULL; idx++)
+	{
+	  if (!(test_binary(targets[idx])))
+	    printf("ERR on %s\n", targets[idx]);
+	}
+    }
+}
+
+
+int	test_binary(char *binary)
+{
+  Elf32_Sym	*sym;
+  elfshobj_t	*obj;
+  void		*ptr;
+  unsigned long	start;
+  unsigned long len;
+  int		ret;
+  int		foffset;
+  int		to_ret;
+
+  char	*sym_name = ".text";
+
+  /** Load elfsh objet */
+  obj = elfsh_load_obj(binary);
+  if (!obj)
+    {
+      printf("ERR failed loading object %s", binary);
+      return(0);
+    }
+  /** Fetch symbol from a string */
+  sym = elfsh_get_metasym_by_name(obj,sym_name);
+  if (!sym)
+    {
+      printf("ERR add elfsh handling error there : metasym failed\n");
+      to_ret = 0;
+      goto leave;
+    }
+  
+  len = sym->st_size;
+  /** Get file offset from that symbol */
+  printf("INFO vaddr = %08X size = %i\n", sym->st_value, sym->st_size);
+  foffset = elfsh_get_foffset_from_vaddr(obj, sym->st_value);
+  printf("INFO file offset of %s = %08X\n", sym_name, foffset);
+
+  ptr = malloc(len);
+  ret = elfsh_raw_read(obj, foffset, ptr, len);
+  printf("INFO raw_read returned %i\n", ret);
+  if (len != ret)
+    {
+      printf("ERR error : read only %i bytes on %i\n", ret, len);
+      to_ret = 0;
+      goto leave;
+    }
+  
+  to_ret = 1;
+ leave:
+  elfsh_unload_obj(obj);
+  return (to_ret);
+}
+
