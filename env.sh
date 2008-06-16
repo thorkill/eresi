e0ab47c114dfad20e146ae22e063a396847928b2
enioh <gh-toec0eiL@eresi-project.org>
2008-06-16 18:48:19 +0000

    summary: Added evarista to env
    Added evarista to environment.

--- env.sh
+++ env.sh
@@ -22,12 +22,15 @@ export LD_LIBRARY_PATH=`pwd`/libaspect:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/libedfmt/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/lib/debug/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/libkernsh/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/evarista/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/testsuite/debugging/binaries-multithreads/:$LD_LIBRARY_PATH
 
 export PATH=`pwd`/elfsh:$PATH
 export PATH=`pwd`/etrace:$PATH
 export PATH=`pwd`/e2dbg:$PATH
 export PATH=`pwd`/kernsh:$PATH
+export PATH=`pwd`/evarista:$PATH
 export PATH=`pwd`:$PATH
+
 echo Environment now ready for testing ERESI without install
 fi
