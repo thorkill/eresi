bbdf1d2fd486c83e45cf1830ecbce4716ecbf55f
pi3 <gh-Ooz8ooda@eresi-project.org>
2008-08-24 22:04:13 +0000

    BUGFIX: #79: Libasm-MIPS still have unsupported instructions

---
+++ libasm/src/arch/mips/handlers/asm_mips_c_ngt_s.c
@@ -0,0 +1,23 @@
+/* Adam 'pi3' Zabrocki */
+/* Manuel Martin - 2007 */
+
+#include <libasm.h>
+
+int asm_mips_c_ngt_s(asm_instr *ins, u_char *buf, u_int len,
+                  asm_processor *proc)
+{
+   struct s_mips_decode_cop2 temp;
+
+   ins->instr = ASM_MIPS_C_NGT_S;
+   ins->type = ASM_TYPE_ARCH | ASM_TYPE_ASSIGN | ASM_TYPE_COMPARISON;
+   mips_convert_format_cop2(&temp,buf);
+   ins->op[0].baser = temp.rd;
+   asm_mips_operand_fetch(&ins->op[0], buf, ASM_MIPS_OTYPE_REGISTER, ins);
+   ins->op[1].baser = temp.rt;
+   asm_mips_operand_fetch(&ins->op[1], buf, ASM_MIPS_OTYPE_REGISTER, ins);
+
+   /* Exceptions: Coprocessor Unusable, Reserved Instruction */
+   /* FPU Exceptions: Unimplemented Operation, Invalid Operation */
+
+   return 4;
+}
---
+++ libasm/src/arch/mips/handlers/asm_mips_c_ole_d.c
@@ -0,0 +1,23 @@
+/* Adam 'pi3' Zabrocki */
+/* Manuel Martin - 2007 */
+
+#include <libasm.h>
+
+int asm_mips_c_ole_d(asm_instr *ins, u_char *buf, u_int len,
+                  asm_processor *proc)
+{
+   struct s_mips_decode_cop2 temp;
+
+   ins->instr = ASM_MIPS_C_OLE_D;
+   ins->type = ASM_TYPE_ARCH | ASM_TYPE_ASSIGN | ASM_TYPE_COMPARISON;
+   mips_convert_format_cop2(&temp,buf);
+   ins->op[0].baser = temp.rd;
+   asm_mips_operand_fetch(&ins->op[0], buf, ASM_MIPS_OTYPE_REGISTER, ins);
+   ins->op[1].baser = temp.rt;
+   asm_mips_operand_fetch(&ins->op[1], buf, ASM_MIPS_OTYPE_REGISTER, ins);
+
+   /* Exceptions: Coprocessor Unusable, Reserved Instruction */
+   /* FPU Exceptions: Unimplemented Operation, Invalid Operation */
+
+   return 4;
+}
