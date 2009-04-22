6cd6be8ce006c33ee2f9c2171885c250040c837a
forger <gh-iu8Ao1Ud@eresi-project.org>
2009-04-22 23:05:54 +0000

    Merging revs 1351 and 1352

--- libgdbwrap/gdbwrapper.c
+++ libgdbwrap/gdbwrapper.c
@@ -119,7 +119,9 @@ static char          *gdbwrap_extract_from_packet(const char *strtoparse,
     charend = charbegin + strlen(charbegin);
   else
     {
-      charend = strstr(charbegin, end);
+      u_int off = (begin == NULL) ? 0 : strlen(begin);
+      
+      charend = strstr(charbegin + off, end);
       if (charend == NULL)
 	return NULL;
     }
@@ -133,7 +135,6 @@ static char          *gdbwrap_extract_from_packet(const char *strtoparse,
 
   return strret;
 }
-
 
 static la32          gdbwrap_little_endian(la32 addr)
 {
@@ -927,15 +928,15 @@ void                gdbwrap_ctrl_c(gdbwrap_t *desc)
  * (default behavior).
  */
 void                 gdbwrap_signal(gdbwrap_t *desc, int signal)
- {
-   char              *rec;
-   char              signalpacket[MSG_BUF];
+{
+  char              *rec;
+  char              signalpacket[MSG_BUF];
 
-   ASSERT(desc != NULL);
-   snprintf(signalpacket, sizeof(signalpacket), "%s;C%.2x",
-	    GDBWRAP_CONTINUEWITH, signal);
-   rec = gdbwrap_send_data(desc, signalpacket);
- }
+  ASSERT(desc != NULL);
+  snprintf(signalpacket, sizeof(signalpacket), "%s;C%.2x",
+	   GDBWRAP_CONTINUEWITH, signal);
+  rec = gdbwrap_send_data(desc, signalpacket);
+}
 
 
 void                 gdbwrap_stepi(gdbwrap_t *desc)
